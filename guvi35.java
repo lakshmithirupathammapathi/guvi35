a43c552d1bd2ff0215af0905780e7af65152b9e1
lakshmithirupathammapathi <41730938+lakshmithirupathammapathi@users.noreply.github.com>
2018-08-04 15:20:45 +0530

    Create guvi35.java

---
+++ guvi35.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String s="helloword.123";
+		    int flag = 0;
+    for (int i = 0; i < s.length(); i++) {
+        if (Character.isDigit(s.charAt(i))) {
+            flag++;
+        }
+    }
+    System.out.println("the numerals are"+flag);
+    return ;
+}
+
+	}
